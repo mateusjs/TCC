410623a4902c7e9f7cd4a5e48fe7e6d29a9dc998
Mateus José <mateusjose19@hotmail.com>
2018-06-15 16:23:40 -0300

    correção de salvamento dos novos arquivos

--- src/bagging.py
+++ src/bagging.py
@@ -18,10 +18,11 @@ class Bagging:
         #lê o CSV
         dataFrame = pd.read_csv(self.csvPath, sep=',')
 
-        #Seleciona todos os valores 1 da coluna Class do dataframe e joga para class1, faz o mesmo para o class2 mas com o 2
-        dfSub = dataFrame.groupby(self.column, as_index=False).apply(lambda x: x.sample(frac=self.percentage, replace=True)).reset_index(drop=True)
+        
 
         for count in range(self.n):
+            #Seleciona todos os valores 1 da coluna Class do dataframe e joga para class1, faz o mesmo para o class2 mas com o 2
+            dfSub = dataFrame.groupby(self.column, as_index=False).apply(lambda x: x.sample(frac=self.percentage, replace=True)).reset_index(drop=True)
             new_path = self.csvNameFinal
             string = str(count)
             print("Gerando SubSets")
